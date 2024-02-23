5ba9437d63e00f6c6625e0e59e043be5eaf18fa6
purvi-tamrakar <124510164+purvi-tamrakar@users.noreply.github.com>
2024-02-23 07:36:44 +0000

    Auto commit on save

--- program2.py
+++ program2.py
@@ -2,8 +2,8 @@ class Solution(object):
     def romanToInt(self, s:str)->int:
         m={
         'I':1, 
-        'I':1,
-        'I':1,
+        'v':1,
+        '':1,
         'I':1,
         'I':1,
         'I':1,
