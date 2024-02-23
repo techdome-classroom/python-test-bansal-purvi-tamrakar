ddc6d19ff6924497a5ce2a558a3f89c792c7d060
purvi-tamrakar <124510164+purvi-tamrakar@users.noreply.github.com>
2024-02-23 08:07:36 +0000

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,7 +2,7 @@ class Solution(object):
     def isValid(self, s:str)->bool:
         if len(s)%2!=0:
             return False
-            dict={'(':')',''}
+            dict={'(':')','['}
         
         pass
     
